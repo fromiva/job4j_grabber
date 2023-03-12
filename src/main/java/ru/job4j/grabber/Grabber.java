25d1ff8ba652b4486b9055a784b0d61195288636
fromiva <66008363+fromiva@users.noreply.github.com>
2023-03-12 23:00:46 +0300

    6. Grabber [#289477]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -1,6 +1,69 @@
 package ru.job4j.grabber;
 
-public class Grabber {
-    public static void main(String[] args) {
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private final Parse parse;
+    private final Store store;
+    private final Scheduler scheduler;
+    private final int time;
+
+    public Grabber(Parse parse, Store store, Scheduler scheduler, int time) {
+        this.parse = parse;
+        this.store = store;
+        this.scheduler = scheduler;
+        this.time = time;
+    }
+
+    @Override
+    public void init() throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInMinutes(time)
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+
+    }
+
+    public static class GrabJob implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            JobDataMap data = context.getJobDetail().getJobDataMap();
+            Store store = (Store) data.get("store");
+            Parse parse = (Parse) data.get("parse");
+            parse.list().forEach(store::save);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties configuration = new Properties();
+        try (InputStream in = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            configuration.load(in);
+        }
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        Parse parse = new HabrCareerParse(new HabrCareerDateTimeParser());
+        Store store = new PsqlStore(configuration);
+        int time = Integer.parseInt(configuration.getProperty("grabber.launch.interval"));
+        new Grabber(parse, store, scheduler, time).init();
     }
 }
